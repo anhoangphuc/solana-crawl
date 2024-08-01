0121508ea63a3ba79ae27098209c374a2577c780
anhoangphuc <hoangphucnb97@gmail.com>
2024-08-01 15:34:53 +0700

    Add action to airdrop to users

--- src/airdrop-to-users.ts
+++ src/airdrop-to-users.ts
@@ -1,7 +1,8 @@
 import fs from 'fs';
 import { getKeypairFromFile } from '@solana-developers/helpers';
-import { getOrCreateAssociatedTokenAccount, getAccount } from 'solana-spl-token';
-import { Connection, PublicKey } from '@solana/web3.js';
+import { getOrCreateAssociatedTokenAccount, getAccount, transfer, getAssociatedTokenAddress } from 'solana-spl-token';
+import { Connection, PUBLIC_KEY_LENGTH, PublicKey } from '@solana/web3.js';
+import { web3 } from '@project-serum/anchor';
 (async () => {
   const totalAmount: Record<string, number> = {};
   const reportData = JSON.parse(fs.readFileSync('./data/report.json', 'utf8'));
@@ -22,10 +23,48 @@ import { Connection, PublicKey } from '@solana/web3.js';
   const connection = new Connection('https://api-mainnet-beta.renec.foundation:8899/');
   const keypair = await getKeypairFromFile('~/.config/renec/deployer_mainnet.json');
 
+
+  // const getBalance: Record<string, boolean> = {};
+  // for (const user in reportData) {
+  //   for (const mint in reportData[user]) {
+  //     if (getBalance[mint] === true)
+  //       continue;
+  //     getBalance[mint] = true;
+  //     console.log("MINT ", mint);
+  //     const tokenAccount = await getOrCreateAssociatedTokenAccount(connection, keypair, new PublicKey(mint), keypair.publicKey);
+  //     console.log("BALANCE ", tokenAccount.amount);
+  //   }
+  // }
+
   for (const user in reportData) {
     for (const mint in reportData[user]) {
       const { amount, symbol, kycLevel, txHashs } = reportData[user][mint];
-      console.log("USER", user, "MINT", mint, "AMOUNT", amount, "SYMBOL", symbol);
+      if (!["reUSD", "RENEC", "reVND"].includes(symbol)) {
+        continue;
+      }
+      console.log("USER", user, "MINT", mint, "AMOUNT", amount, symbol, "KYC LEVEL ", kycLevel);
+      if (symbol === "RENEC") {
+        const transaction = new web3.Transaction().add(
+          web3.SystemProgram.transfer({
+            fromPubkey: keypair.publicKey,
+            toPubkey: new PublicKey(user),
+            lamports: amount,
+          })
+        );
+        const txHash  = await web3.sendAndConfirmTransaction(connection, transaction, [keypair]);
+        console.log("Transfer success at txHash ", txHash);
+      } else {
+        const source = await getAssociatedTokenAddress(new PublicKey(mint), keypair.publicKey);
+        const destination = await getAssociatedTokenAddress(new PublicKey(mint), new PublicKey(user));
+        const txHash = await transfer(connection, keypair, source, destination, keypair, amount);
+        console.log("Transfer success at txHash", txHash);
+      }
+      await delay(3000);
     }
   }
 })();
+
+function delay(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
